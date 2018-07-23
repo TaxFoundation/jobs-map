14b7b21880e315707c138337f838ee50ab61605a
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-07-23 09:59:05 -0400

    Add main component files

---
+++ src/components/USMap.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { geoAlbersUsa, geoPath } from 'd3-geo';
+import { feature } from 'topojson-client';
+import US from '../data/us.json';
+
+export default class USMap extends Component {
+  render() {
+    return <div />;
+  }
+}
