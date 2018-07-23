b509d9b3b4cf6fb717aa9a05d607564e7a1e3a5e
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-07-23 11:59:41 -0400

    Make do with crappy HTML5 input range support

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,32 +2,45 @@ import React, { Compinent } from 'react';
 import JobsMapContext from '../Context';
 import styled from 'styled-components';
 
-const Range = styled.input``;
+const SliderContainer = styled.div`
+  display: grid;
+  grid-template: auto / 1fr 5fr 1fr;
+`;
+
+const RangeInput = styled.input`
+  width: 100%;
+`;
+
+const Label = styled.div`
+  text-align: center;
+`;
 
 const Slider = () => {
   return (
     <JobsMapContext.Consumer>
       {context => (
-        <div>
-          <Range
-            type="range"
-            list="years"
-            min={Math.min(...context.years)}
-            max={Math.max(...context.years)}
-            step="1"
-            value={context.state.currentYear}
-            onChange={e => context.updateState('currentYear', +e.target.value)}
-          />
-          <datalist id="years">
-            {context.years.map((y, i) => (
-              <option
-                value={y}
-                key={`range-${y}`}
-                label={i === 0 || i === context.years.length - 1 ? y : ''}
-              />
-            ))}
-          </datalist>
-        </div>
+        <SliderContainer>
+          <Label>{Math.min(...context.years)}</Label>
+          <div>
+            <RangeInput
+              type="range"
+              list="years"
+              min={Math.min(...context.years)}
+              max={Math.max(...context.years)}
+              step="1"
+              value={context.state.currentYear}
+              onChange={e =>
+                context.updateState('currentYear', +e.target.value)
+              }
+            />
+            <datalist id="years">
+              {context.years.map((y, i) => (
+                <option value={y} key={`range-${y}`} />
+              ))}
+            </datalist>
+          </div>
+          <Label>{Math.max(...context.years)}</Label>
+        </SliderContainer>
       )}
     </JobsMapContext.Consumer>
   );
--- src/components/USMap.js
+++ src/components/USMap.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { geoAlbersUsa, geoPath } from 'd3-geo';
 import { feature } from 'topojson-client';
