4ae961e4bcc62673a9f2b57ef78cf7d2c5840120
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-07-23 09:58:48 -0400

    Set up context api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import JobsMapContext from './Context';
+
+class Provider extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      year: 2018,
+    };
+  }
+
+  render() {
+    return (
+      <JobsMapContext.Provider
+        value={{
+          state: this.state,
+          updateState: (pieceOfState, data) => {
+            this.setState({
+              [pieceOfState]: data,
+            });
+          },
+        }}
+      >
+        {this.props.children}
+      </JobsMapContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <Provider>
+        <div />
+      </Provider>
+    );
   }
 }
 
---
+++ src/Context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const JobsMapContext = createContext();
+
+export default JobsMapContext;
